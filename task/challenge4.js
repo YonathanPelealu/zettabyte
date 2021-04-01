f17f60d4e885a39d77df1d8df55983d80440f8c9
andrepelealu <andre02.9d@gmail.com>
2021-04-01 20:27:18 +0700

    add reference

--- task/challenge4.js
+++ task/challenge4.js
@@ -36,3 +36,10 @@ closed(emitter);
 module.exports.createEmitter = createEmitter;
 module.exports.opened = opened;
 module.exports.closed = closed;
+
+/**
+ * Reference : 
+ * 1) https://www.youtube.com/watch?v=l20MBBFZAmA
+ * 2) https://www.youtube.com/watch?v=9ErAONqE6HE
+ * 3) http://www.sukmasaputra.com/2018/09/memahami-events-eventemitter-dalam.html#:~:text=Node.js%20Events%20EventEmitter%20adalah,berkala%20disebut%20events%20yang%20menyebabkan
+ */
