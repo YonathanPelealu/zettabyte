a7ee7ff14b31c250a4416887015efd203084dbce
andrepelealu <andre02.9d@gmail.com>
2021-04-01 20:55:59 +0700

    task 3

--- task/challenge3.js
+++ task/challenge3.js
@@ -1,6 +1,9 @@
-function getInParallel(apiCalls) {
+async function getInParallel(apiCalls) {
     // Write your code here
+    for (const item of apiCalls){
+        return await item;
     }
+}
     let promise = getInParallel([() => Promise.resolve("First API call!"),
     () => Promise.resolve("Second API call!")]);
     
@@ -8,3 +11,12 @@ function getInParallel(apiCalls) {
     promise.then((result) => console.log(result)).catch((err) => console.log(err));
     }
     module.exports.getInParallel = getInParallel;
+
+    /**
+     * getInParallel dikasih 2 param api
+     * harus di await satu2 di dalem function getInParallel
+     * kalo api call sudah selesai , maka tampilka di dalam if(promise)
+     *  for (const item of apiCalls) -> buat looping kalo pake for of , 
+     *  - dia nunggu satu request bener2 selesai baru lanjut ke request selanjutnya
+     *  - jadi antar request pertama dan kedua tidak saling interupt
+     */ 
